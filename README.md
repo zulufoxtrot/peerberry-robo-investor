fa921aa037a7c7c342fe1324de6b5e4b5b2edb80
zulufoxtrot <youri.lcd@gmail.com>
2023-07-17 21:39:28 +0200

    Exception handling; logging; readme

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-# peerberry-robo-investor
+# Peerberry Robo-investor
 Invests automatically on the Peerberry crowdlending platform
+
+Esketit already has a robo-investor built-in, but for some reason it does not invest fast enough, causing cash drag.
+
+# Install
+
+Requires python 3.9+
+
+Install Poetry:
+
+`curl -sSL https://install.python-poetry.org | python -`
+
+Setup project with poetry:
+
+`poetry install`
+
+# Run
+
+Set up a cron job for this command:
+
+`PEERBERRY_EMAIL=your_email PEERBERRY_PASSWORD=your_password python /path/to/project/main.py`
 
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import logging
 import os
+import sys
+import time
 from logging.handlers import RotatingFileHandler
 
 from peerberrypy import API
-from peerberrypy.exceptions import InsufficientFunds
+from peerberrypy.exceptions import InsufficientFunds, TooManyRequestsException
 
 if __name__ == "__main__":
 
@@ -23,23 +25,41 @@ if __name__ == "__main__":
     )
     logging.info("Logged in.")
 
+    available_balance = api_client.get_overview()["availableMoney"]
+    logging.info(f"Available balance: {available_balance}€")
+    if not available_balance >= min(10, AMOUNT_TO_BUY):
+        logging.info("Available balance below 10€ or below the purchase amount. Aborting.")
+        sys.exit()
+
+    # take a 20% margin (some loans will become unavailable for purchase by the time we try to purchase them
+    loans_needed = int((available_balance * 1.2) // AMOUNT_TO_BUY)
+
     loans = api_client.get_loans(min_interest_rate=10,
                                  max_remaining_term=60,
                                  group_guarantee=True,
                                  exclude_invested_loans=True,
                                  sort="interest_rate",
-                                 quantity=100,
+                                 quantity=loans_needed,
                                  raw=True)
 
-    logging.info(f"Found {len(loans)} loans matching criteria")
+    logging.info(f"Found {len(loans)} loans matching criteria (max requested: {loans_needed})")
 
     for loan in loans:
-        logging.info(f"Purchasing #" + loan["loanId"])
+        logging.info(f"Purchasing #" + str(loan["loanId"]))
         try:
-            api_client.purchase_loan(loan["loanId"], AMOUNT_TO_BUY)
-        except InsufficientFunds:
-            logging.info("No funds left. Halting purchases.")
-            break
+            api_client.purchase_loan(loan["loanId"], 10)
+        except TooManyRequestsException:
+            logging.error("Too many API requests. Sleeping for 1 minute, then resuming purchases")
+            time.sleep(60)
+            continue
+        except InsufficientFunds as e:
+            # known cases:
+            # - The remaining available loan amount after investment cannot be less than the minimum investment amount
+            # - The loan is sold out
+            logging.info("Failed buying, skipping")
+            logging.info(f"Error message: {e}")
+            continue
+        time.sleep(2)
 
     api_client.logout()
     logging.info("Logged out.")
